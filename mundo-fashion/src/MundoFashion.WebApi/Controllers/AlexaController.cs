4dfcc271a1aab59505c5558ebe47a1a32f11803a
andreisalvador <andreifs95@gmail.com>
2021-06-10 17:02:14 -0300

    Adicionado consultas para alexa

--- mundo-fashion/src/MundoFashion.WebApi/Controllers/AlexaController.cs
+++ mundo-fashion/src/MundoFashion.WebApi/Controllers/AlexaController.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using MundoFashion.Core.Notifications;
+using MundoFashion.Core.Utils;
 using MundoFashion.Domain;
 using MundoFashion.Domain.Repositories;
 using MundoFashion.WebApi.Controllers.Base;
@@ -15,10 +17,12 @@ namespace MundoFashion.WebApi.Controllers
     {
         private readonly IMemoryCache _cache;
         private readonly IUsuarioRepository _usuarioRepository;
-        public AlexaController(INotificationHandler<Notificacao> notificacoes, IMemoryCache cache, IUsuarioRepository usuarioRepository) : base(notificacoes)
+        private readonly ISolicitacaoRepository _solicitacaoRepository;
+        public AlexaController(INotificationHandler<Notificacao> notificacoes, IMemoryCache cache, IUsuarioRepository usuarioRepository, ISolicitacaoRepository solicitacaoRepository) : base(notificacoes)
         {
             _cache = cache;
             _usuarioRepository = usuarioRepository;
+            _solicitacaoRepository = solicitacaoRepository;
         }
 
         [HttpPost]
@@ -51,12 +55,12 @@ namespace MundoFashion.WebApi.Controllers
         public async Task<ActionResult<bool>> VerificarUsuarioRegistrado(string alexaUserId)
         {
             if (string.IsNullOrWhiteSpace(alexaUserId))
-                return BadRequest("As informações enviadas não podem ser vazia, entre em contato com a administração do time notes.");
+                return BadRequest("As informações enviadas não podem ser vazia, entre em contato com a administração do mundo fashion.");
 
             return RespostaCustomizada(await _usuarioRepository.ObterUsuarioPorAlexaUserId(alexaUserId).ConfigureAwait(false) != null);
         }
 
-        [HttpGet]
+        [HttpGet, Authorize]
         [Route("obter-passcode-cadastrar-alexa")]
         public async Task<ActionResult<string>> ObterPasscodeCadastrarAlexa()
         {
@@ -65,6 +69,32 @@ namespace MundoFashion.WebApi.Controllers
             _cache.Set<Guid>(passcode, Guid.NewGuid(), TimeSpan.FromMinutes(1));
 
             return await Task.FromResult(passcode);
+        }
+
+        [HttpGet]
+        [Route("listar-solicitacoes-usuario-tomador")]
+        public async Task<ActionResult<long[]>> ListarSolicitacoesUsuarioTomador(string alexaUserId)
+        {
+            if (string.IsNullOrWhiteSpace(alexaUserId))
+                BadRequest("As informações enviadas não podem ser vazia, entre em contato com a administração do mundo fashion.");
+
+            Usuario usuario = await _usuarioRepository.ObterUsuarioPorAlexaUserId(alexaUserId).ConfigureAwait(false);
+            long[] codigos = (await _solicitacaoRepository.ObterSolicitacoes(s => s.TomadorId == usuario.Id).ConfigureAwait(false)).Select(s => s.Codigo).ToArray();
+
+            return RespostaCustomizada(codigos);
+        }
+
+        [HttpGet]
+        [Route("obter-detalhes-solicitacoes")]
+        public async Task<ActionResult<string>> ObterDetalhesSolicitacao(string alexaUserId, long codigo)
+        {
+            if (string.IsNullOrWhiteSpace(alexaUserId) || codigo <= 0)
+                BadRequest("As informações enviadas não podem ser vazia, entre em contato com a administração do mundo fashion.");
+
+            Usuario usuario = await _usuarioRepository.ObterUsuarioPorAlexaUserId(alexaUserId).ConfigureAwait(false);
+            Solicitacao solicitacao = (await _solicitacaoRepository.ObterSolicitacoes(s => s.TomadorId == usuario.Id && s.Codigo == codigo).ConfigureAwait(false)).SingleOrDefault();
+
+            return RespostaCustomizada($"A solicitação esta com status {EnumUtils.ObterValorEmTexto(solicitacao.Status)}");
         }
 
         private string GetPasscode()
