1e70a82fd2d592ce51b1fd5de75108ccc05fe9c5
andreisalvador <andreifs95@gmail.com>
2021-06-20 19:45:18 -0300

    Ajuste model

--- mundo-fashion/src/MundoFashion.WebApi/Models/Usuario/PrestadorTomadorModel.cs
+++ mundo-fashion/src/MundoFashion.WebApi/Models/Usuario/PrestadorTomadorModel.cs
@@ -5,8 +5,10 @@ namespace MundoFashion.WebApi.Models.Usuario
     public record PrestadorTomadorModel
     {
         public Guid Id { get; set; }
+        public string AvatarLink { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }                
         public string Cpf { get; set; }
+        public string DescricaoPessoal { get; set; }
     }
 }
