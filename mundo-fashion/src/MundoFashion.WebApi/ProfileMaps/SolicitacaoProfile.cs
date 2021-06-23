bcfbbeb9baf0dbc41fac2265277c235134eb89a2
andreisalvador <andreifs95@gmail.com>
2021-06-23 13:58:36 -0300

    Ajuste solicitação

--- mundo-fashion/src/MundoFashion.WebApi/ProfileMaps/SolicitacaoProfile.cs
+++ mundo-fashion/src/MundoFashion.WebApi/ProfileMaps/SolicitacaoProfile.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using AutoMapper;
 using MundoFashion.Core.Utils;
 using MundoFashion.Domain;
 using MundoFashion.WebApi.Models;
+using MundoFashion.WebApi.Models.Mensagem;
 using MundoFashion.WebApi.Models.Solicitacao;
 using MundoFashion.WebApi.Models.Usuario;
 
@@ -14,7 +16,8 @@ namespace MundoFashion.WebApi.ProfileMaps
         {
             CreateMap<Solicitacao, SolicitacaoModel>()
                 .ForMember(s => s.Status, s => s.MapFrom(x => EnumUtils.ObterValorEmTexto(x.Status)))    
-                .AfterMap((src, dest, context) => dest.Tomador = context.Mapper.Map<Usuario, PrestadorTomadorModel>(src.Tomador));                
+                .AfterMap((src, dest, context) => dest.Tomador = context.Mapper.Map<Usuario, PrestadorTomadorModel>(src.Tomador))
+                .AfterMap((src, dest, context) => dest.Mensagens = context.Mapper.Map<List<Mensagem>, List<MensagemModel>>(src.Mensagens.OrderByDescending(x => x.CreatedAt).ToList()));                
                         
         }
     }
